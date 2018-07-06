44c2b2874a1189b5080275777d76ea3f7f081939
user862 <jobmail862@gmail.com>
2018-07-06 20:57:05 +0300

    delete old service

--- Services/vk_service/src/model/json_models.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AppSettings {
-    pub client_id: String,
-    pub client_secret: String,
-    pub db_host: String,
-    pub db_name: String,
-    pub db_user: String,
-    pub db_password: String,
-}
--- Services/vk_service/src/model/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod json_models;
-pub mod db_models;
-pub mod data_models;
